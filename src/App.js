047486dcd0deb301d1ad79e7e299ae253c978a70
egoing <egoing@gmail.com>
2020-01-24 12:38:12 +0900

    6. 로딩 기능의 구현과 수업을 마치며

--- src/App.js
+++ src/App.js
@@ -36,41 +36,75 @@ class Article extends Component{
     );
   }
 }
-
+class NowLoading extends Component{
+  render(){
+    return <div>Now Loading....</div>
+  }
+}
 class App extends Component {
   state = {
-    article:{title:'Welcome', desc:'Hello, React & Ajax'},
-    list:[]
+    article:{
+      item:{title:'Welcome', desc:'Hello, React & Ajax'},
+      isLoading:false
+    },
+    list:{
+      items:[],
+      isLoading:false
+    }
   }
   componentDidMount(){
+    var newList = Object.assign({}, this.state.list, {isLoading:true});
+    this.setState({list:newList});
     fetch('list.json')
       .then(function(result){
         return result.json();
       })
       .then(function(json){
         console.log(json);
-        this.setState({list:json});
+        this.setState({list:{
+          items:json,
+          isLoading:false
+        }});
       }.bind(this));
   }
   render(){
+    var NavTag = null;
+    if(this.state.list.isLoading){
+      NavTag = <NowLoading></NowLoading>
+    } else {
+      NavTag = <Nav list={this.state.list.items} onClick={function(id){
+        var newArticle = Object.assign({}, this.state.article, {isLoading:true});
+        this.setState({article:newArticle});
+        fetch(id+'.json')
+          .then(function(result){
+            return result.json();
+          })
+          .then(function(json){
+            this.setState({
+              article:{
+                item:{
+                  title:json.title,
+                  desc:json.desc
+                },
+                isLoading:false
+              }
+            })
+          }.bind(this));
+      }.bind(this)}></Nav>
+    }
+
+    var ArticleTag = null;
+    if(this.state.article.isLoading){
+      ArticleTag = <NowLoading></NowLoading>
+    } else {
+      ArticleTag = <Article title={this.state.article.item.title} desc={this.state.article.item.desc}></Article>
+    }
+
     return (
       <div className="App">
         <h1>WEB</h1>
-        <Nav list={this.state.list} onClick={function(id){
-          fetch(id+'.json')
-            .then(function(result){
-              return result.json();
-            })
-            .then(function(json){
-              this.setState({
-                article:{
-                  title:json.title,
-                  desc:json.desc
-                }
-              })
-            }.bind(this));
-        }.bind(this)}></Nav>
-        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+        {NavTag}
+        {ArticleTag}
       </div>
     )
   }
