58d7f122ad6327223c967471f9b623e731bb00b0
egoing <egoing@gmail.com>
2020-01-24 10:17:49 +0900

    4. ajax로 컴포넌트의 상태 변경하기

--- src/App.js
+++ src/App.js
@@ -18,7 +18,16 @@ class Nav extends Component {
     var listTag = [];
     for(var i=0; i<this.state.list.length; i++){
       var li = this.state.list[i];
-      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
+      listTag.push(
+        <li key={li.id}>
+          <a href={li.id} data-id={li.id} onClick={function(e){
+            e.preventDefault();
+            console.log('trigger');
+            this.props.onClick(e.target.dataset.id);
+          }.bind(this)}>
+            {li.title}
+          </a>
+        </li>)
     }
     return (
       <nav>
@@ -30,17 +39,43 @@ class Nav extends Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
-      <Nav></Nav>
+class Article extends Component{
+  render(){
+    return(
       <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    article:{title:'Welcome', desc:'Hello, React & Ajax'}
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        <Nav onClick={function(id){
+          fetch(id+'.json')
+            .then(function(result){
+              return result.json();
+            })
+            .then(function(json){
+              this.setState({
+                article:{
+                  title:json.title,
+                  desc:json.desc
+                }
+              })
+            }.bind(this));
+        }.bind(this)}></Nav>
+        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+      </div>
+    )
+  }
 }
 
 export default App;
