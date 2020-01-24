3b7fe02b0f1b9301074734d4d56551d47cd65214
egoing <egoing@gmail.com>
2020-01-24 10:01:23 +0900

    3. 컴포넌트 초기화 때 ajax 사용하기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  state = {
+    list:[]
+  }
+  componentDidMount(){
+    fetch('list.json')
+      .then(function(result){
+        return result.json();
+      })
+      .then(function(json){
+        console.log(json);
+        this.setState({list:json});
+      }.bind(this));
+  }
+  render(){
+    var listTag = [];
+    for(var i=0; i<this.state.list.length; i++){
+      var li = this.state.list[i];
+      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
+    }
+    return (
+      <nav>
+        <ul>
+          {listTag}
+        </ul>
+      </nav>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <h1>WEB</h1>
-      <nav>
-        <ul>
-          <li><a href="1">HTML</a></li>
-          <li><a href="2">CSS</a></li>
-          <li><a href="3">JS</a></li>
-        </ul>
-      </nav>
+      <Nav></Nav>
       <article>
         <h2>Welcome</h2>
         Hello, React &amp; Ajax
